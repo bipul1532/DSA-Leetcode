9d3cfe6d2ca6df19208740098e7e64f3950fcc9e
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2024-07-20 13:45:41 +0530

    Time: 16 ms (86.67%) | Memory: 27.5 MB (43.73%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& arr, int d) {
-        int n = arr.size();
-        if(n==0){
-            return ;
-        }
-        d = d % n;  // Adjust d if it's larger than the size of the array
-        vector<int> temp(d);
-
-        // Store the last d elements in the temporary array
-        for (int i = 0; i < d; i++) {
-            temp[i] = arr[n - d + i];
-        }
-
-        // Shift the first n-d elements to the right
-        for (int i = n - 1; i >= d; i--) {
-            arr[i] = arr[i - d];
-        }
-
-        // Copy the elements from the temporary array to the beginning of the array
-        for (int i = 0; i < d; i++) {
-            arr[i] = temp[i];
-        }
+        int n=arr.size();
+        d=d%n;
+        reverse(arr.begin(),arr.end());
+        reverse(arr.begin(),arr.begin()+d);
+        reverse(arr.begin()+d,arr.end());
     }
 };
