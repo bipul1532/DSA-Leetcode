bde423a37337311d42c3f7e671f252990c0ce7d0
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2024-07-22 11:25:25 +0530

    Time: 13 ms (53.71%) | Memory: 14.6 MB (12.67%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,17 +3,14 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int,int>mpp;
         int req;
-        int i;
-        int num;
         for(int i=0;i<nums.size();i++){
-            req=target-nums[i];
-            if( mpp.find(req)!=mpp.end()){
-            return {mpp[req],i};
+            int num=nums[i];
+            req=target-num;
+            if(mpp.find(req)!=mpp.end()){
+                return{ mpp[req],i};
+            }
+            mpp[num]=i;
         }
-        mpp[nums[i]]=i;
-        }
-        
-
         return {-1,-1};
     }
 };
