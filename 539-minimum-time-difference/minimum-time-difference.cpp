17344350862fc45a7dba97c4428cd9d934959139
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2024-08-01 13:45:24 +0530

    Time: 13 ms (54.54%) | Memory: 17.6 MB (39.57%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -2,24 +2,20 @@ class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
         vector<int>minutes;
-        //step 1: convert time string into minutes integer value
         for(int i=0;i<timePoints.size();i++){
-            string curr=timePoints[i];
-            int hours=stoi(curr.substr(0,2)); //strting from zero index upto 2 length
-            int min=stoi(curr.substr(3,2));  //string from third index upto 2 length
+            int hours=stoi(timePoints[i].substr(0,2));
+            int min=stoi(timePoints[i].substr(3,2));
             int totalminutes=hours*60+min;
             minutes.push_back(totalminutes);
         }
-        //step2:sort
+        //step 2:-----
         sort(minutes.begin(),minutes.end());
-        //step3: difference and calculate min difference
         int mini=INT_MAX;
-        int n=minutes.size();
-        for(int i=0;i<n-1;i++){
-            int diff=minutes[i+1]-minutes[i];
-            mini=min(diff,mini);
+        for(int i=0;i<minutes.size()-1;i++){
+           int minm= minutes[i+1]-minutes[i];
+           mini=min(mini,minm);
         }
-        //imp missing part
+        int n=minutes.size();
         int lastdiff=(minutes[0]+1440)-minutes[n-1];
         mini=min(lastdiff,mini);
         return mini;
