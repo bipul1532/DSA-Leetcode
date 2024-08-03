e03d6826ce01081777038cf493452044cca11054
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2024-08-03 13:02:35 +0530

    Time: 8 ms (60.01%) | Memory: 8.5 MB (45.87%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,10 +3,8 @@ public:
     bool isPalindrome(int n) {
         int dup = n;
         long rev = 0;
-        if (n < 0) {
-            return false;
-        }
-        while (n != 0) {
+        
+        while (n > 0) {
             int lastdigit = n % 10;
             rev = (rev * 10) + lastdigit;
             n = n / 10;
