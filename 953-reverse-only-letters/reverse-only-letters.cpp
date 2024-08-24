041f3d0dce841d01901126209afa61c34847a406
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2024-08-24 19:54:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (44.48%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.cpp
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-        // use two pointer approach
+        int n = s.size();
         int i = 0;
-        int j = s.length() - 1;
-        while (i < j) {
+        int j = s.size() - 1;
+        while (i <= j) {
             if (isalpha(s[i]) && isalpha(s[j])) {
                 swap(s[i], s[j]);
                 i++;
                 j--;
-            } else if (!isalpha(s[i])) {
+            }else if(!isalpha(s[i])){
                 i++;
-            } else {
+            }else{
                 j--;
             }
         }
