857c92047e286003049843aafaea27ad8ae03f4a
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2024-08-24 19:40:38 +0530

    Time: 7 ms (75.84%) | Memory: 8.9 MB (50.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()){
+        if(s.size()!=t.size()){
             return false;
         }
         int hash[256]={0};
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.size();i++){
             hash[s[i]]++;
-        }
-        for(int i=0;i<t.length();i++){
             hash[t[i]]--;
         }
         for(int i=0;i<256;i++){
