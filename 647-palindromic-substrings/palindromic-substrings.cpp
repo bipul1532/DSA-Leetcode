0b737756b9883a7c198cd85f389fc4cd1668b108
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2024-08-24 18:18:02 +0530

    Time: 1322 ms (7.39%) | Memory: 407.1 MB (10.19%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int expandAroundIndex(string s,int i,int j){
-        int count=0;
-        while(i>=0 && j<s.length() && s[i]==s[j]){
-            count++;
-            i--;
-            j++;
+    bool checkPalindrome(string str,int i,int j) {
+        while (i <= j) {
+            if (str[i] != str[j]) {
+                return false;
+            }
+            i++;
+            j--;
         }
-        return count;
+        return true;
     }
     int countSubstrings(string s) {
-        int count=0;
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            //for odd
-            int oddkaAns=expandAroundIndex(s,i,i);
-            count=oddkaAns+count;
-            //for even
-            int evenkaAns=expandAroundIndex(s,i,i+1);
-            count=evenkaAns+count;
+        int cnt=0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (checkPalindrome(s, i, j)) {
+                    cnt++;
+                }
+            }
         }
-        return count;
+
+        return cnt;
     }
 };
