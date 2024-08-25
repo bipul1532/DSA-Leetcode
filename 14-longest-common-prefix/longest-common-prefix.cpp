58213eaa6cf137c55b904b1281eb606742283b6a
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2024-08-25 09:00:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (81.95%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,8 +3,8 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string ans="";
         for(int i=0;i<strs[0].size();i++){
-            for(auto s:strs){
-                if(strs[0][i]!=s[i]){
+            for(int j=1;j<strs.size();j++){
+                if(strs[0][i]!=strs[j][i]){
                     return ans;
                 }
             }
