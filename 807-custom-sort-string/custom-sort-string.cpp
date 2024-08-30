5d3b936c44929230298f38a0c92b10efec636caa
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2024-08-30 15:17:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (32.59%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     string customSortString(string order, string s) {
         vector<int>hash(26,-1);
-
-        for(int i=0;i<order.length();i++){
-            char ch=order[i];
-            hash[ch-'a']=i;
+        for(int i=0;i<order.size();i++){
+            hash[order[i]-'a']=i;
         }
 
-        auto myComparator = [&hash](char& ch1,char& ch2){
+        auto mycomparator=[&hash](char& ch1,char& ch2){
             return hash[ch1-'a']<hash[ch2-'a'];
         };
-        sort(s.begin(),s.end(),myComparator);
+        sort(s.begin(),s.end(),mycomparator);
         return s;
     }
 };
