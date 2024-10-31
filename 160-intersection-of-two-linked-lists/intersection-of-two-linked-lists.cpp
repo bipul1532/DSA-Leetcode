d056ae58126f79d3df1f42b53e4b837384510b4e
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2024-10-31 18:24:29 +0530

    Time: 37 ms (63.56%) | Memory: 17.4 MB (23.69%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,20 +8,35 @@
  */
 class Solution {
 public:
+    ListNode* collisionFound(ListNode* smallerHead,ListNode* largerHead,int d){
+        while(d){
+            d--;
+            largerHead=largerHead->next;
+        }
+        while(smallerHead!=largerHead){
+            smallerHead=smallerHead->next;
+            largerHead=largerHead->next;
+        }
+        return smallerHead;
+    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        map<ListNode*,int>mpp;
-        ListNode* temp=headA;
-        while(temp!=nullptr){
-            mpp[temp]=1;
-            temp=temp->next;
+        ListNode* t1=headA;
+        int n1=0;
+        ListNode* t2=headB;
+        int n2=0;
+        while(t1!=NULL){
+            n1++;
+            t1=t1->next;
         }
-        temp=headB;
-        while(temp!=nullptr){
-            if(mpp.find(temp)!=mpp.end()){
-                return temp;
-            }
-            temp=temp->next;
+        while(t2!=NULL){
+            n2++;
+            t2=t2->next;
         }
-        return NULL;
+        if(n1<n2){
+            return collisionFound(headA,headB,n2-n1);
+        }else{
+            return collisionFound(headB,headA,n1-n2);
+        }
+        return nullptr;
     }
 };
