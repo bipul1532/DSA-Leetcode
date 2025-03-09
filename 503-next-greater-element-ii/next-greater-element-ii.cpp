59ac340fa0c706a0e804eefc9f55a456c6bcfa6c
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2025-03-09 19:02:14 +0530

    Time: 104 ms (7.44%) | Memory: 27.9 MB (97.16%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-//circular interay array 
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> nge(n, -1); // Initialize with -1
-        
-        // Double loop for brute-force approach
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < n; j++) { // Search next greater element
-                int idx = (i + j) % n; // Circular indexing
-                if (nums[idx] > nums[i]) {
-                    nge[i] = nums[idx];
-                    break; // Stop at first greater element
+        int n=nums.size();
+        vector<int>nge(n,-1);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<i+n;j++){
+                int index=j%n;
+                if(nums[index]>nums[i]){
+                    nge[i]=nums[index];
+                    break;
                 }
             }
         }
