244d21ea313abd0e25acea4be383f52b1fdeaea5
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2025-03-09 22:51:54 +0530

    Time: 119 ms (5.01%) | Memory: 28 MB (94.84%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
         vector<int>nge(n,-1);
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<i+n;j++){
+        for(int i=0;i<=n-1;i++){
+            for(int j=i+1;j<=i+n-1;j++){
                 int index=j%n;
                 if(nums[index]>nums[i]){
                     nge[i]=nums[index];
