36118cd6762b14256721fdb4bd225eb117ea75f5
BIPUL KUMAR <139461997+bipul1532@users.noreply.github.com>
2025-03-11 20:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (20.42%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n=height.size();
-        if(n==0) return 0;
-        int l=0;
-        int r=n-1;
-        int lmax=0,rmax=0,total=0;
-        while(l<r){
-            if(height[l]<=height[r]){
-                if(height[l]<lmax){
-                    total+=lmax-height[l];
-                }else{
-                    lmax=height[l];
-                }
-                l++;
-            }
-            else{
-                if(rmax>height[r]){
-                    total+=rmax-height[r];
-                }else{
-                    rmax=height[r];
-                }
-                r--;
-            }
+    vector<int> leftmax(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> prefix(n);
+        prefix[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = max(prefix[i - 1], arr[i]);
         }
+        return prefix;
+    }
+
+    vector<int> rightmax(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> suffix(n);
+        suffix[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = max(suffix[i + 1], arr[i]);
+        }
+        return suffix;
+    }
+
+    int trap(vector<int>& arr) {
+        int n = arr.size();
+        if (n == 0) return 0;
+
+        vector<int> lmax = leftmax(arr);
+        vector<int> rmax = rightmax(arr);
+
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += min(lmax[i], rmax[i]) - arr[i];
+        }
+
         return total;
     }
 };
